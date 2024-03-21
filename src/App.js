720701ba831faa115bc285f48beb067ed482e046
HLouLusk <hannmacd@gmail.com>
2024-03-20 22:30:17 -0700

    CMS setup for blog. Routing for the blog has been made.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import CreatePost from "./pages/create-post";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login"> Login</Link>
+        <Link to="/createpost">Create Post</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/createpost" element={<createPost />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
