6f45b6bbf9265bfccf0876402fc0a09f5142ecb3
HLouLusk <hannmacd@gmail.com>
2024-03-25 20:11:15 -0700

    Header component created and new routes added

---
+++ src/pages/Blog/Blog.jsx
@@ -0,0 +1,5 @@
+const Blog = () => {
+  return <div className="Blog">Blog Page</div>;
+};
+
+export default Blog;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="Home">Home Page</div>;
+};
+
+export default Home;
