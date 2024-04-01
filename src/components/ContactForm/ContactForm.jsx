43e5b7670f27f69febfa5d16ef62a744ae79a792
HLouLusk <hannmacd@gmail.com>
2024-03-31 18:02:52 -0700

    email and lead creation works

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RangeSlider from "react-range-slider-input";
+import emailjs from "@emailjs/browser";
 import "react-range-slider-input/dist/style.css";
+
+import { db } from "../../firebase-config";
+import { addDoc, collection } from "firebase/firestore";
+
 import "./ContactForm.scss";
 import { CALLSTOACTION } from "../../assets/copy/contactForm";
 
@@ -13,6 +18,24 @@ const ContactForm = () => {
   const [callToAction, setCallToAction] = useState(
     CALLSTOACTION[Math.floor(Math.random() * CALLSTOACTION.length)]
   );
+  const [showAlert, setShowAlert] = useState(false);
+  const leadsCollectionRef = collection(db, "leads");
+
+  const addLead = async () => {
+    const leadData = {
+      name,
+      email,
+      budget: budget[1],
+      description,
+      website,
+      contacted: Date.now(),
+    };
+    try {
+      const newDocRef = await addDoc(leadsCollectionRef, leadData);
+    } catch (error) {
+      console.error("Error saving lead:", error);
+    }
+  };
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -36,24 +59,46 @@ const ContactForm = () => {
     setWebsite(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Budget:", budget);
-    setName("");
-    setEmail("");
-    setBudget(0);
+    try {
+      await addLead();
+      await emailjs.sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        event.target,
+        { publicKey: process.env.REACT_APP_PUBLIC_KEY }
+      );
+      setName("");
+      setEmail("");
+      setBudget([0, 5000]);
+      setDescription("");
+      setWebsite("");
+    } catch (error) {
+      setShowAlert(true);
+    }
   };
+
+  useEffect(() => {
+    if (!showAlert) return;
+
+    const timer = setTimeout(() => {
+      setShowAlert(false);
+    }, 10000); // Hide the alert after 10 seconds
+
+    return () => clearTimeout(timer);
+  }, [showAlert]);
 
   return (
     <form id="contact-form" className="contact-form" onSubmit={handleSubmit}>
+      {showAlert && <h3>Oops! Something went wrong.</h3>}
       <h2 className="section__title">{callToAction}</h2>
       <div>
         <label>
           Name <span className="contact-label--required">*</span>
         </label>
         <input
+          name="to_name"
           className="contact-input"
           type="text"
           value={name}
@@ -66,6 +111,7 @@ const ContactForm = () => {
           Email <span className="contact-label--required">*</span>
         </label>
         <input
+          name="to_email"
           className="contact-input"
           type="email"
           value={email}
@@ -79,6 +125,7 @@ const ContactForm = () => {
           Budget: ${budget[1].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
         </label>
         <RangeSlider
+          name="budget"
           className="single-thumb"
           max={20000}
           step={2000}
@@ -93,6 +140,7 @@ const ContactForm = () => {
           Project Description <span className="contact-label--required">*</span>
         </label>
         <textarea
+          name="description"
           className="contact-input contact-input--auto-resize"
           value={description}
           onChange={handleDescriptionChange}
@@ -102,13 +150,14 @@ const ContactForm = () => {
       <div>
         <label>Website</label>
         <input
+          name="website"
           className="contact-input"
           type="text"
           value={website}
           onChange={handleWebsiteChange}
-          required
         />
       </div>
+      {showAlert && <p>Oops! Something went wrong.</p>}
       <button className="call-to-action" type="submit">
         Submit
       </button>
